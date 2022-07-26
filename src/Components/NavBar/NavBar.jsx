a3693708eb6e6322b1576d13e789bfdf101d458d
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-07-26 10:49:05 -0300

    first commit on jsx components, created and sized

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        NavBar
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
