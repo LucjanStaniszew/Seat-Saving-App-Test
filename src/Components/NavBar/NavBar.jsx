bf26d7ea90b2d49b2e805fe9769210126b6162fd
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-07-26 11:24:13 -0300

    NavBar modified and Stylized

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,9 +4,16 @@ import './NavBar.css'
 const NavBar = () => {
   return (
     <header>
-      <nav>
-        NavBar
+      <nav className='navigation'>
+        <a href="/">
+          <img src="Assets/iveco.png" alt="Iveco Van" className='logo'/>
+        </a>
+        <h1 className='title'>Seat Reservation App</h1>
+        <a href="https://lucjan-staniszew-portfolio-lucjanstaniszew.vercel.app/" target="blank">
+          <img src="Assets/portfolio.png" alt="Portfolio" className='me' />
+        </a>
       </nav>
+
     </header>
   )
 }
